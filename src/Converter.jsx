dd9142ddf8d096af9f381c039ef291715797982b
Naseem Raad <nzraad@gmail.com>
2021-10-13 19:20:48 -0500

    Debug

--- src/Converter.jsx
+++ src/Converter.jsx
@@ -63,18 +63,18 @@ const Converter = ({
     });
   };
 
-  useEffect(() => {
-    console.log(decimal);
-    // axios.get(`/api/converter?decimal=${decimalValue}`).then((response) => {
-    //   const { data } = response;
-    //   setDecimal(data.decimalRepr);
-    //   setHexadecimal(data.hexadecimalRepr);
-    //   setExponentArray(data.exponent_array);
-    //   setMantisseArray(data.mantisse_array);
-    //   setSign(data.sign_bool);
-    //   setIsLoading(false);
-    // });
-  }, [decimalValue]);
+  // useEffect(() => {
+  //   console.log(decimal);
+  //   // axios.get(`/api/converter?decimal=${decimalValue}`).then((response) => {
+  //   //   const { data } = response;
+  //   //   setDecimal(data.decimalRepr);
+  //   //   setHexadecimal(data.hexadecimalRepr);
+  //   //   setExponentArray(data.exponent_array);
+  //   //   setMantisseArray(data.mantisse_array);
+  //   //   setSign(data.sign_bool);
+  //   //   setIsLoading(false);
+  //   // });
+  // }, [decimalValue]);
 
   const onChangeHex = () => {
     axios.get(`/api/converter?hexadecimal=${hexadecimal}`).then((response) => {
