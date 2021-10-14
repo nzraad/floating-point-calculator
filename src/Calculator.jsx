c6e5c19d06d23da363e03a73e47b2d6f625a838a
Naseem Raad <nzraad@gmail.com>
2021-10-13 19:26:20 -0500

    Debug

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react/cjs/react.development";
+import React, { useState } from "react";
 import axios from "axios";
 
 import { Panel, Button } from "react95";
